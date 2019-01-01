a240f289991627ec01c310b6101b2aff27a58673
Yasuaki Uechi <y@uechi.io>
2019-01-01 11:15:50 +0900

    add settings

--- src/components/SettingsView.js
+++ src/components/SettingsView.js
@@ -1,36 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-export default class SettingsView extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: '',
-    }
+export default function SettingsView() {
+  const [token, setToken] = useState(() => localStorage.getItem('token'))
+
+  function onSave() {
+    console.log(token)
+    localStorage.setItem('token', token)
   }
 
-  onChange(event) {
-    const {
-      currentTarget: { value },
-    } = event
-    this.setState({ value })
-  }
-
-  onSubmit() {
-    console.log('submit', this.state.value)
-    localStorage.setItem('token', this.state.value)
-  }
-
-  render() {
-    return (
-      <Container>
-        <input type="text" onChange={this.onChange.bind(this)} />
-        <button onClick={this.onSubmit.bind(this)}>Save</button>
-        <Link to="/">Close</Link>
-      </Container>
-    )
-  }
+  return (
+    <Container>
+      <input
+        type="text"
+        onChange={e => setToken(e.target.value)}
+        defaultValue={token}
+      />
+      <div>{token}</div>
+      <button onClick={onSave}>Save</button>
+      <Link to="/">Close</Link>
+    </Container>
+  )
 }
 
 const Container = styled.div`
